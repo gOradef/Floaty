579665d372d9a7b33c2f5cc700b1a05eb9738ec1
goradef <efimovsasha2007@gmail.com>
2023-11-20 15:04:36 +0300

    move libs dir to root (/)

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
+    set(CMAKE_INSTALL_CONFIG_NAME "")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -44,7 +44,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/home/goradef/Floaty/build/src/server_route/cmake_install.cmake")
+  include("/home/goradef/Floaty/build/src/cmake_install.cmake")
 
 endif()
 
