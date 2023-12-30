e9316ea834691a5caac6a0f675dfc48798ffbba1
Sanya <99015750+gOradef@users.noreply.github.com>
2023-12-30 15:57:36 +0300

    Update crow.cpp

--- src/server_route/crow.cpp
+++ src/server_route/crow.cpp
@@ -125,7 +125,7 @@ int main()
     {
         return handleErrPage(404);
     });
-    app .bindaddr("127.0.0.1")
+    app .bindaddr("62.233.46.131")
         .port(6010)
         .multithreaded()
         .run_async();
