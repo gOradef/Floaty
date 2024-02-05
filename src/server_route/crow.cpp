f6d977cef95c6e28156859c4e1c7dd57ef94712e
Floaty Server <floaty@7edu24.ru>
2024-02-05 22:32:46 +0300

    localhost and 6010 port

--- src/server_route/crow.cpp
+++ src/server_route/crow.cpp
@@ -164,7 +164,7 @@ int main()
     {
         return handleErrPage(404);
     });
-    app .bindaddr("62.233.46.131")
+    app .bindaddr("127.0.0.1")
         .port(6010)
         .multithreaded()
         .run_async();
