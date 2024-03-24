bb46aedbd852744fbdfbad5b823172034b097350
gOradef <pi4fous8j@mozmail.com>
2024-03-24 19:21:01 +0300

    Add overlay, fix stings on input for amount data in Form,

--- src/server_route/crow.cpp
+++ src/server_route/crow.cpp
@@ -85,6 +85,13 @@ public:
             return res.end();
         });
 
+        //! Только для html
+        CROW_ROUTE(app, "/<string>")
+                ([](std::string file) {
+                    if (file == "userForm" || file == "userInterface") return handleErrPage(0, "no access");
+                    else return genWebPages(file);
+                });
+
         //Region API
         CROW_ROUTE(app, "/api/form")
                 .methods(crow::HTTPMethod::POST)
@@ -138,12 +145,6 @@ public:
         CROW_CATCHALL_ROUTE(app)([] {
             return handleErrPage(404);
         });
-	//! Тол�Lко дл�O html
-        CROW_ROUTE(app, "/<string>")
-                ([](std::string file) {
-                    if (file == "userForm" || file == "userInterface") return handleErrPage(0, "no access");
-                    else return genWebPages(file);
-                });
     }
     [[maybe_unused]]
     static void useSSL() {
@@ -162,9 +163,9 @@ crow::SimpleApp Server::app;
 int main()
 {
 //    62.233.46.131
-    Server server("62.233.46.131", 443);
-    Server::useSSL();
-//    Server server("127.0.0.1", 443);
+//    Server server("62.233.46.131", 443);
+//    server.useSSL();
+    Server server("127.0.0.1", 443);
     server.initRoutes();
     Server::run();
     return 0;
