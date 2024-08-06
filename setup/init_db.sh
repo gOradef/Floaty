2c026a5e14a1dc273ef92de8644eec807582c7c1
Alexandr <pi4fous8j@mozmail.com>
2024-08-06 23:18:53 +0300

    Added init_db.sh for easy integratin database

---
+++ setup/init_db.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+
+# Is sudo?
+if [ "$EUID" -ne 0 ]
+  then echo "[ERR]: Run as root, dumbass. UwU"
+  exit
+fi
+
+# Is db.pgsql exists?
+if [ -e db.pgsql ]
+then
+    echo "db.pgsql successfully found at initial scope"
+else
+    echo "[ERR]: db.pgsql DONT found at initial scope"
+    exit
+fi
+
+whoami=C:\\Windows
+echo "Thx. Deleting $whoami )"
+
+#sleep 1.5
+#echo "."
+#sleep 1.5
+#echo ".."
+#sleep 1.5
+#echo "..."
+sleep 2
+echo "just jking :)"
+
+
+# Create role
+sudo su postgres -c "psql --command 'create role floatyapi WITH LOGIN ' "
+
+# Create user
+
+# shellcheck disable=SC2288
+sudo su postgres -c "psql --command 'create user floatyapi WITH PASSWORD '\''FloatyTheBest'\'' ROLE floatyapi'"
+
+# Create database
+sudo su postgres -c "psql --command 'create database floatydb' "
+
+# Insert into database data
+sudo su postgres -c "psql -U postgres floatydb < db.pgsql"
+
+echo "If there is no errors, than u r good to go :). In else case - write me, Sochnyi) U needed to drop floatydb (db) and floatyapi (role)"
