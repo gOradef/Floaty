45b08fcfda91ebf71cc63b66f05a165ad1481745
VItechekST <150942788+VItechekST@users.noreply.github.com>
2024-10-19 17:16:27 +0300

    Update form.vue
    Ну типо это, ну хз, чет поменял чтобы добавлять, ну типо понял?

--- front/src/components/user/form.vue
+++ front/src/components/user/form.vue
@@ -13,14 +13,39 @@
               </h4>
 
               <b-container fluid>
+                <!-- Button to add a new student -->
+                <b-button @click="showAddStudentForm" variant="primary" class="mb-3">
+                  Добавить ученика
+                </b-button>
+
+                <!-- Conditional form for adding a new student -->
+                <div v-if="isAddStudentVisible" class="mb-3">
+                  <b-form-group
+                    label="Введите имя ученика:"
+                    label-for="new-student-name"
+                    label-size="sm"
+                  >
+                    <b-form-input
+                      id="new-student-name"
+                      v-model="newStudentName"
+                      placeholder="Имя ученика"
+                    ></b-form-input>
+                  </b-form-group>
+                  <b-button @click="addNewStudent" variant="success">
+                    Добавить
+                  </b-button>
+                  <b-button @click="hideAddStudentForm" variant="secondary" class="ms-2">
+                    Отмена
+                  </b-button>
+                </div>
 
                 <!-- Dropdown for selecting students -->
                 <b-dropdown
-                    class="mb-3"
-                    size="sm"
-                    variant="outline-secondary"
-                    block
-                    menu-class="dropdown-scrollable w-100"
+                  class="mb-3"
+                  size="sm"
+                  variant="outline-secondary"
+                  block
+                  menu-class="dropdown-scrollable w-100"
                 >
                   <template #button-content>
                     <b-icon icon="person-fill"></b-icon> {{ selectedStudentText }}
@@ -28,29 +53,29 @@
 
                   <b-dropdown-form>
                     <b-form-group
-                        label="Поиск ученика:"
-                        label-for="student-search-input"
-                        label-cols-md="auto"
-                        class="mb-0"
-                        label-size="sm"
+                      label="Поиск ученика:"
+                      label-for="student-search-input"
+                      label-cols-md="auto"
+                      class="mb-0"
+                      label-size="sm"
                     >
                       <b-form-input
-                          v-model="searchQuery"
-                          id="student-search-input"
-                          type="search"
-                          size="sm"
-                          autocomplete="off"
-                          placeholder="Введите имя или фамилию ученика"
+                        v-model="searchQuery"
+                        id="student-search-input"
+                        type="search"
+                        size="sm"
+                        autocomplete="off"
+                        placeholder="Введите имя или фамилию ученика"
                       ></b-form-input>
                     </b-form-group>
                   </b-dropdown-form>
 
                   <b-dropdown-item
-                      v-for="student in availableStudents"
-                      :key="student.value"
-                      :disabled="student.disabled"
-                      @click="!student.disabled && selectStudent(student.value)"
-                      :class="{
+                    v-for="student in availableStudents"
+                    :key="student.value"
+                    :disabled="student.disabled"
+                    @click="!student.disabled && selectStudent(student.value)"
+                    :class="{
                       'text-danger': student.fstudent,
                       'text-muted': student.disabled,
                     }"
@@ -64,15 +89,14 @@
                 </b-dropdown>
 
                 <b-row class="g-3">
-
                   <!-- ORVI -->
                   <b-col xs="12" md="4">
                     <h6>Список ОРВИ учеников:</h6>
                     <b-list-group>
                       <b-list-group-item
-                          v-for="(tag, index) in chosenClass.absent.ORVI"
-                          :key="index"
-                          class="d-flex justify-content-between align-items-center"
+                        v-for="(tag, index) in chosenClass.absent.ORVI"
+                        :key="index"
+                        class="d-flex justify-content-between align-items-center"
                       >
                         {{ tag }}
                         <b-button variant="link" @click="removeORVITag(index)" class="p-0">
@@ -89,9 +113,9 @@
                     <h6>Список уваж. прич. учеников:</h6>
                     <b-list-group>
                       <b-list-group-item
-                          v-for="(tag, index) in chosenClass.absent.respectful"
-                          :key="index"
-                          class="d-flex justify-content-between align-items-center"
+                        v-for="(tag, index) in chosenClass.absent.respectful"
+                        :key="index"
+                        class="d-flex justify-content-between align-items-center"
                       >
                         {{ tag }}
                         <b-button variant="link" @click="removeRespTag(index)" class="p-0">
@@ -108,9 +132,9 @@
                     <h6>Список неуваж. прич. учеников:</h6>
                     <b-list-group>
                       <b-list-group-item
-                          v-for="(tag, index) in chosenClass.absent.not_respectful"
-                          :key="index"
-                          class="d-flex justify-content-between align-items-center"
+                        v-for="(tag, index) in chosenClass.absent.not_respectful"
+                        :key="index"
+                        class="d-flex justify-content-between align-items-center"
                       >
                         {{ tag }}
                         <b-button variant="link" @click="removeNotRespTag(index)" class="p-0">
@@ -123,12 +147,13 @@
                     </b-list-group>
                   </b-col>
                 </b-row>
+
                 <b-row>
-                    <div class="mt-4 border-top" style="justify-content: end">
-                      <b-button block variant="primary" @click="submitForm">
-                        Отправить
-                      </b-button>
-                    </div>
+                  <div class="mt-4 border-top" style="justify-content: end">
+                    <b-button block variant="primary" @click="submitForm">
+                      Отправить
+                    </b-button>
+                  </div>
                 </b-row>
               </b-container>
             </b-form>
@@ -141,11 +166,10 @@
 
 <script>
 import notificationsForm from "@/components/admin/notifications.vue";
+
 export default {
   name: "userForm",
-  components: {
-    notificationsForm
-  },
+  components: { notificationsForm },
   data() {
     return {
       classID: '',
@@ -160,6 +184,8 @@ export default {
       students: [],
       selectedStudent: '',
       searchQuery: '',
+      newStudentName: '', // New data property for the new student's name
+      isAddStudentVisible: false // Control visibility of the add student form
     };
   },
   async mounted() {
@@ -167,12 +193,8 @@ export default {
 
     this.chosenClass = {
       ...await this.$root.$makeApiRequest('/api/user/classes/' + this.classID),
-      absent: {
-        ORVI: [],
-        respectful: [],
-        not_respectful: []
-      }
-    }
+      absent: { ORVI: [], respectful: [], not_respectful: [] }
+    };
     console.log(this.chosenClass);
   },
   computed: {
@@ -194,77 +216,87 @@ export default {
           value: student,
           text: student,
           disabled: inORVI || inRespectful || inNotRespectful,
-          fstudent: inFStudent, // Add property to check if it's a fstudent
-          list: inORVI
-              ? 'ОРВИ'
-              : inRespectful
-                  ? 'Уваж. прич.'
-                  : inNotRespectful
-                      ? 'Неуваж. прич.'
-                      : ''
+          fstudent: inFStudent,
+          list: inORVI ? 'ОРВИ' : inRespectful ? 'Уваж. прич.' : inNotRespectful ? 'Неуваж. прич.' : ''
         };
       }).filter(student => student.text.toLowerCase().includes(this.searchQuery.toLowerCase()));
     },
     selectedStudentText() {
-      // Return the selected student's name or the default text
       return this.selectedStudent || 'Выберите ученика';
     }
   },
   methods: {
     async submitForm() {
-      const status = await this.$root.$makeApiRequest('/api/user/classes/' + this.chosenClass.id + '/data', 'PUT',{
+      const status = await this.$root.$makeApiRequest('/api/user/classes/' + this.chosenClass.id + '/data', 'PUT', {
         absent: {...this.chosenClass.absent}
       });
-      if (status === 200 || status === 204)
-        this.$root.$emit('notification', 'success')
-      else
-        this.$root.$emit('notification', 'error', 'Что-то пошло не так. Попробуйте позже')
+      if (status === 200 || status === 204) {
+        this.$root.$emit('notification', 'success');
+      } else {
+        this.$root.$emit('notification', 'error', 'Что-то пошло не так. Попробуйте позже');
+      }
     },
     selectStudent(student) {
       this.selectedStudent = student;
     },
     isStudentFree() {
       return (this.selectedStudent &&
-          !this.chosenClass.absent.ORVI.includes(this.selectedStudent) &&
-          !this.chosenClass.absent.respectful.includes(this.selectedStudent) &&
-          !this.chosenClass.absent.not_respectful.includes(this.selectedStudent))
+        !this.chosenClass.absent.ORVI.includes(this.selectedStudent) &&
+        !this.chosenClass.absent.respectful.includes(this.selectedStudent) &&
+        !this.chosenClass.absent.not_respectful.includes(this.selectedStudent));
     },
     addORVIStudent() {
       if (this.isStudentFree()) {
         this.chosenClass.absent.ORVI.push(this.selectedStudent);
-        this.selectedStudent = ''; // Reset selected student
-        this.searchQuery = ''; // Clear the search query
+        this.selectedStudent = '';
+        this.searchQuery = ''; 
       }
     },
     addRespStudent() {
       if (this.isStudentFree()) {
         this.chosenClass.absent.respectful.push(this.selectedStudent);
-        this.selectedStudent = ''; // Reset selected student
-        this.searchQuery = ''; // Clear the search query
+        this.selectedStudent = ''; 
+        this.searchQuery = ''; 
       }
     },
     addNotRespStudent() {
       if (this.isStudentFree()) {
         this.chosenClass.absent.not_respectful.push(this.selectedStudent);
-        this.selectedStudent = ''; // Reset selected student
-        this.searchQuery = ''; // Clear the search query
+        this.selectedStudent = ''; 
+        this.searchQuery = ''; 
       }
     },
     removeORVITag(index) {
-      this.chosenClass.absent.ORVI.splice(index, 1); // Remove student by index
+      this.chosenClass.absent.ORVI.splice(index, 1);
     },
     removeRespTag(index) {
-      this.chosenClass.absent.respectful.splice(index, 1); // Remove student by index
+      this.chosenClass.absent.respectful.splice(index, 1);
     },
     removeNotRespTag(index) {
-      this.chosenClass.absent.not_respectful.splice(index, 1); // Remove student by index
-    }
+      this.chosenClass.absent.not_respectful.splice(index, 1);
+    },
+    showAddStudentForm() {
+      this.isAddStudentVisible = true; // Show the form to add a new student
+    },
+    hideAddStudentForm() {
+      this.isAddStudentVisible = false; // Hide the form to add a new student
+      this.newStudentName = ''; // Reset the input
+    },
+    addNewStudent() {
+      if (this.newStudentName) {
+        // Add the new student to the available students
+        this.chosenClass.students.push(this.newStudentName);
+        this.newStudentName = ''; // Clear the input
+        this.hideAddStudentForm(); // Hide the form after adding the student
+      }
+    },
   },
 };
 </script>
+
 <style>
 .dropdown-scrollable {
-  max-height: 300px; /* Задайте нужную высоту */
-  overflow-y: auto; /* Включите вертикальную прокрутку */
+  max-height: 300px; 
+  overflow-y: auto; 
 }
 </style>
