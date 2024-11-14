0aaca38b7a476c234a849e0781bc16b979feedb8
Alexandr <pi4fous8j@mozmail.com>
2024-11-14 21:18:37 +0300

    [qualityOfLife] slightly changed dataBackups

--- setup/backupTables.sh
+++ setup/backupTables.sh
@@ -6,7 +6,8 @@ current_date=$(date +"%d-%m-%y")
 # Construct the filename with the current date
 filename="backup - $current_date.pgsql"
 
+mkdir backups/
 # Perform the pg_dump command
-pg_dump -U postgres floatydb -t schools -t schools_classes -t schools_classes_ownership -t schools_data -t schools_invites -t schools_invites_archived -t schools_template_classes -t schools_users -t users -t users_salts > "$filename"
+pg_dump -U postgres floatydb -t schools -t schools_classes -t schools_classes_ownership -t schools_data -t schools_invites -t schools_invites_archived -t schools_template_classes -t schools_users -t users -t users_salts > backups/"$filename"
 
 echo "Backup completed. File saved as: $filename"
