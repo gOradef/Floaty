e3f24d712681ecea85cb91e0af33d0073307e622
Alexandr <pi4fous8j@mozmail.com>
2024-12-08 00:05:57 +0300

    edited frontend for updating user name

--- front/src/components/admin/context-forms/users/editUser.vue
+++ front/src/components/admin/context-forms/users/editUser.vue
@@ -67,18 +67,28 @@ export default {
         this.roles.push('teacher')
       if (this.isAdmin)
         this.roles.push('admin')
+
       let classes_ids = [];
       this.entity_buff.classes.map(classt => {
         classes_ids.push(classt.id);
       })
 
-      const status = await this.$root.$makeApiRequest('/api/org/users/' + this.entity.id,
-      'PUT',
-          {
-            classes: classes_ids,
-            roles: this.roles
-          }
+      const requestBody = {
+        classes: classes_ids,
+        roles: this.roles,
+      };
+
+      // Add the name field if this.newUserName is not empty
+      if (this.newUserName && this.newUserName.trim() !== '') {
+        requestBody.name = this.newUserName;
+      }
+
+      const status = await this.$root.$makeApiRequest(
+          '/api/org/users/' + this.entity.id,
+          'PUT',
+          requestBody
       );
+
       if (status === 204)
         this.$root.$emit('notification', 'success');
       else
@@ -112,7 +122,7 @@ export default {
   <b-tabs v-model="currentTabIndex" nav-class="mb-4">
 
     <!--    Region Rename -->
-    <b-tab disabled title="Переименовать"> <!--! DISABLED -->
+    <b-tab title="Переименовать"> <!--! DISABLED -->
       <b-form>
         <b-input
           v-model="newUserName"
@@ -120,8 +130,7 @@ export default {
         ></b-input>
         <b-card-text class="text-center mt-2">
           {{ entity_buff.name }} <b-icon icon="arrow-right"></b-icon>
-          {{ newUserName }}</b-card-text
-        >
+          {{ newUserName }}</b-card-text>
       </b-form>
     </b-tab>
     <!-- Region Classes -->
