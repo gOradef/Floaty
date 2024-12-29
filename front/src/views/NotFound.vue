29f1cbe0a1e74e7651712197f423a0ffb8adc3fd
Alexandr <pi4fous8j@mozmail.com>
2024-12-29 14:03:24 +0300

    [new] 404 page

--- front/src/views/NotFound.vue
+++ front/src/views/NotFound.vue
@@ -1,7 +1,20 @@
 <template>
   <div>
-    <b>Not Found. 404.</b> <br>
-    That url doesnt exists.
+    <b-jumbotron
+        fluid>
+
+      <template #header>
+        404
+      </template>
+      <template #lead>
+
+        <p style="font-size: 2rem">
+          Ой... Страница не существует
+        </p>
+        <span> Мы не смогли найти данную страницу для вас.</span>
+
+      </template>
+    </b-jumbotron>
   </div>
 </template>
 
