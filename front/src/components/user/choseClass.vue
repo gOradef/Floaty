6a4e59b958c8562a14e7fa748a87552620bde03e
gOradef <efimovsasha2007@gmail.com>
2025-02-02 00:06:24 +0300

    add text if classHandler doesnt have any awailable classes in owning

--- front/src/components/user/choseClass.vue
+++ front/src/components/user/choseClass.vue
@@ -6,17 +6,22 @@ export default {
   data() {
     return {
       loadingClasses: false,
+      isClassExists: true,
       classes: [],
     }
   },
   async mounted() {
     const res = await this.$root.$makeApiRequest('/api/user/classes');
-    this.classes = res.classes;
+    if (res.classes !== null) //if is not null, so no available class
+      this.classes = res.classes;
+    else {
+      this.isClassExists = false;
+    }
+    this.loadingClasses = false;
   },
   created() {
     this.loadingClasses = true;
     this.classes = []
-    this.loadingClasses = false;
   },
   methods: {
     chooseClass(classt) {
@@ -38,7 +43,7 @@ export default {
   >
   <b-card title="Выберите класс">
       <b-overlay :show="loadingClasses">
-        <b-list-group >
+        <b-list-group v-if="isClassExists">
           <b-list-group-item
               v-for="(classt, index) in classes"
               :key="index"
@@ -52,6 +57,10 @@ export default {
           {{ classt.name }}
           </b-list-group-item>
         </b-list-group>
+        <div v-else>
+          <p>Похоже, что у вас нет доступа ни к одному классу. </p>
+          <p>Если это поведение непредвиденно, то обратитесь к администратору организации.</p>
+        </div>
       </b-overlay>
     </b-card>
   </div>
