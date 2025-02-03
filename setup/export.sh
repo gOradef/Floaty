d6052d7e199889b1a6eb88fe473f8e8c49fe22c5
gOradef <efimovsasha2007@gmail.com>
2025-02-04 00:22:40 +0300

    [internal] update export.sh

--- setup/export.sh
+++ setup/export.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-mkdir ../deploy
+mkdir ../.deploy
 
 echo '--- Started Vue prepare'
 cd ../front
 npm run build
 
-cp ./dist ./public ../deploy -r
+cp ./dist ./public ../.deploy -r
 
 echo '--- Started API prepare'
-mkdir ../deploy/api
+mkdir ../.deploy/api
 cd ../api
-cp CMakeLists.txt cmake include lib src ../deploy/api -r
+cp CMakeLists.txt cmake include lib src ../.deploy/api -r
 
 echo '--- Started configs prepare'
 cd ..
-cp run services setup ./deploy -r
+cp run services setup config.json ./.deploy -r
 
+tar -zcvf deploy.tar.gz .deploy/
